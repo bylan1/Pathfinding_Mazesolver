6d8cc5660eb8e08c0a64315def049f105e9130cb
bylan1 <dthtb85@gmail.com>
2024-07-10 14:17:59 +0930

    initial tests, starting to learn masks

--- inputhandler.py
+++ inputhandler.py
@@ -2,7 +2,9 @@ import math
 import numpy as np
 from skimage import io
 import cv2
+import matplotlib.pyplot as plt
 
+# For image loading
 def load(img_path):
     """Loads an image from the file path using skimage.io.imread
     Converting pixel values between 0.0 and 1.0
@@ -14,10 +16,11 @@ def load(img_path):
         out: numpy array of shape(image_height, image_width, 3)
     """
     out = None
-    out = io.imread(img_path)/255
+    out = io.imread(img_path)
     
     return out
 
+# For visual analysis
 def display(img, caption=''):
     """Displays an image given details
 
@@ -35,6 +38,7 @@ def display(img, caption=''):
     plt.axis('off')
     plt.show()
 
+# For compression analysis
 def print_stats(image):
     """Prints the height, width and number of channels in an image.
         
@@ -51,17 +55,66 @@ def print_stats(image):
     
     return None
 
+# For easier point finding
+def high_saturation(image):
+    out = image.copy()
+
+    red_mask = (image[:, :, 0] > 127) & (image[:, :, 1] < 127) & (image[:, :, 2] < 127)
+    green_mask = (image[:, :, 0] < 127) & (image[:, :, 1] > 127) & (image[:, :, 2] < 127)
+    blue_mask = (image[:, :, 0] < 127) & (image[:, :, 1] < 127) & (image[:, :, 2] > 127)
+    black_mask = (image[:, :, 0] < 127) & (image[:, :, 1] < 127) & (image[:, :, 2] < 127)
+    white_mask = (image[:, :, 0] > 127) & (image[:, :, 1] > 127) & (image[:, :, 2] > 127)
+
+    # Apply the masks to set the appropriate colors
+    out[red_mask] = [255, 0, 0]
+    out[green_mask] = [0, 255, 0]
+    out[blue_mask] = [0, 0, 255]
+    out[black_mask] = [0, 0, 0]
+    out[white_mask] = [255, 255, 255]
+
+    return out
+
+# For key point finding
 def find_points(image):
-    """Scan image for coloured points
+    """Scan image for red and green points
 
     Inputs:
-        image: numpy array of shape(image_height, image_width, n_channels).
+        image: numpy array of shape(image_height, image_width, n_channels)
 
     Returns:
         coords: array of x and y location of coloured points in array
     """
+    coords = []
+    red_c = 0
+    green_c = 0
 
-    return None
+    for row in range(image.shape[0]):
+        for col in range(image.shape[1]):
+            pixel = image[row, col]
+
+            if pixel[0] > 127 and pixel[1] < 50 and pixel[2] < 50:
+                image[row,col] = [255, 255, 255]
+                if red_c == 0:
+                    coords.append((row, col))
+                    red_c = 1
+            if pixel[0] < 50 and pixel[1] > 127 and pixel[2] < 50:
+                image[row,col] = [255, 255, 255]
+                if green_c == 0:
+                    coords.append((row, col))
+                    green_c = 1
+    
+    return coords
+
+# def compress(image):
+#     """Compresses image into lower quality for convenient processing
+
+#     Inputs:
+#         image: numpy array of shape(image_height, image_width, n_channels)
+
+#     Returns:
+#         out: numpy array of shape(image_height, image_width, n_channels)
+#         identify appropriate height and width
+#     """
 
 def binary(input_image, threshold):
     """Converts a RGB image into binary
@@ -73,15 +126,33 @@ def binary(input_image, threshold):
     Returns:
         out: binary numpy array with shape `(output_rows, output_cols)`
     """
-    out = None
-
-    temp = input_image.copy()
+    out = input_image.copy()
 
     for row in range(input_image.shape[0]):
         for col in range(input_image.shape[1]):
-            temp[row, col] = np.multiply(temp[row, col] > threshold, 1)
-
-    out = temp
+            out[row, col] = np.multiply(out[row, col] > threshold, 1)
 
     return out
 
+def preprocess(image, threshold):
+    """Applies preliminary point finding, compression and binary conversion to image
+
+    Inputs:
+        image: numpy array of shape(image_height, image_width, n_channels).
+        threshold: limit value for binary preprocessing
+
+    Returns:
+        out: binary numpy array with shape `(output_rows, output_cols)`
+        coords: array of x and y location of coloured points in array
+    """
+    out = None
+    coords = []
+
+    return out, coords
+
+image1 = load('paths/blankpath.png')
+image1 = high_saturation(image1)
+display(image1, 'Blank Path with red and green points')
+coords = find_points(image1)
+print(coords)
+display(image1, 'Blank Path with red and green points')
